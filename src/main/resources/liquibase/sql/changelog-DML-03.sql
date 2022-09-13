1bd93f1ca51d76614ead060b388416ca8ce812dd
freesky <freesky_zh@hotmail.com>
2022-09-13 21:28:57 +0800

    add more sql in one sql file

---
+++ src/main/resources/liquibase/sql/changelog-DML-03.sql
@@ -0,0 +1,23 @@
+--liquibase formatted sql
+
+-- changeset max:01
+-- another update
+update liquibase.persons
+set birthday='2012-02-02'
+where id=5
+;
+
+-- changeset max:02
+-- another update
+update liquibase.persons
+set birthday='2013-03-03'
+where id=10
+;
+
+
+-- changeset max:03
+-- another update
+update liquibase.persons
+set birthday='2000-03-03'
+where id=1
+;
